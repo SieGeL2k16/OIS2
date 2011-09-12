fcf48e3571f5da77ded2a0dfa3bde3a6e76d5fec
Sascha 'SieGeL' Pfalz <webmaster@saschapfalz.de>
2011-09-12 20:17:05 +0000

    12-Sep-2011 - Process details now display every column in the V$SESSION               column, including new fields added in 9i, 10g and 11g.
    git-svn-id: svn://localhost/OIS2@5 7a03d42c-cd42-4f2b-90a3-7b965337538a

--- ext/showprocesses/show_processes_details.php
+++ ext/showprocesses/show_processes_details.php
@@ -4,7 +4,7 @@
  * Displays detailed informations for a given process id.
  * @package OIS2
  * @author Sascha 'SieGeL' Pfalz <php@saschapfalz.de>
- * @version 2.00 (31-May-2009)
+ * @version 2.01 (12-Sep-2011)
  * $Id$
  * @filesource
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
@@ -13,6 +13,7 @@
  */
 define('IS_EXTENSION' , 1);
 require_once('../../inc/sessionheader.inc.php');
+require_once('command_list.inc.php');
 
 $sid    = (isset($_GET['SID']))     ? strip_tags($_GET['SID']) : '';
 $serial = (isset($_GET['SERIAL']))  ? strip_tags($_GET['SERIAL']) : '';
@@ -24,13 +25,26 @@ $OIS2EXT->PrintExtHeader('Show process details for SID='.$sid.' AND SERIAL#='.$s
 ?>
 <div id="fullpage_content">
 <?php
+$addq = '';
+if($OIS2EXT->Get_Oracle_Version() >= 9)
+  {
+  $addq.= 'CURRENT_QUEUE_DURATION,CLIENT_IDENTIFIER,';
+  }
+if($OIS2EXT->Get_Oracle_Version() >=10)
+  {
+  $addq.= 'BLOCKING_SESSION_STATUS,BLOCKING_INSTANCE,BLOCKING_SESSION,SEQ#,EVENT#,EVENT,P1,P1TEXT,P2,P2TEXT,P3,P3TEXT,WAIT_CLASS_ID,WAIT_CLASS#,WAIT_CLASS,WAIT_TIME,SECONDS_IN_WAIT,STATE,SERVICE_NAME,SQL_TRACE,SQL_TRACE_WAITS,SQL_TRACE_BINDS,';
+  }
+if($OIS2EXT->Get_Oracle_Version() >=11)
+  {
+  $addq.= 'SQL_TRACE_PLAN_STATS,SESSION_EDITION_ID,RAWTOHEX(CREATOR_ADDR) AS CREATOR_ADDR,CREATOR_SERIAL#,ECID,';
+  }
 $myquery =<<<EOM
 SELECT RAWTOHEX(SADDR) AS SADDR,
        SID,
-       SERIAL# AS SERIALNR,
+       SERIAL#,
        NVL(AUDSID,0) AS AUDSID,
        RAWTOHEX(PADDR) AS PADDR,
-       USER# AS USERNR,
+       USER#,
        USERNAME,
        COMMAND,
        OWNERID,
@@ -38,9 +52,10 @@ SELECT RAWTOHEX(SADDR) AS SADDR,
        NVL(LOCKWAIT,'---') AS LOCKWAIT,
        STATUS,
        SERVER,
-       SCHEMA# AS SCHEMANR,
+       SCHEMA#,
        SCHEMANAME,
        OSUSER,
+       PROCESS,
        MACHINE,
        NVL(TERMINAL,'---') AS TERMINAL,
        PROGRAM,
@@ -48,12 +63,25 @@ SELECT RAWTOHEX(SADDR) AS SADDR,
        NVL(MODULE,'---') AS MODULE,
        NVL(ACTION,'---') AS ACTION,
        NVL(CLIENT_INFO,'---') AS CLIENT_INFO,
-       TO_CHAR(LOGON_TIME,'DD-Mon-YYYY HH24:MI:SS') AS LTIME,
+       TO_CHAR(LOGON_TIME,'DD-Mon-YYYY HH24:MI:SS') AS LOGON_TIME,
+       LAST_CALL_ET,
+       FIXED_TABLE_SEQUENCE,
+       ROW_WAIT_OBJ#,
+       ROW_WAIT_FILE#,
+       ROW_WAIT_BLOCK#,
+       ROW_WAIT_ROW#,
+       FAILOVER_TYPE,
+       FAILOVER_METHOD,
+       FAILED_OVER,
+       RESOURCE_CONSUMER_GROUP,
+       PDML_STATUS,
+       PDDL_STATUS,
+       PQ_STATUS,
+       $addq
        RAWTOHEX(SQL_ADDRESS) AS SQL_ADDRESS,
        SQL_HASH_VALUE,
        RAWTOHEX(PREV_SQL_ADDR) AS PREV_SQL_ADDR,
-       PREV_HASH_VALUE,
-       PROCESS
+       PREV_HASH_VALUE
        FROM V\$SESSION
  WHERE SID=:mysid
    AND SERIAL#=:myserial
@@ -82,146 +110,6 @@ if(isset($d['PREV_SQL_ADDR']) && isset($d['PREV_HASH_VALUE']))
     }
   $db->FreeResult();
   }
-$cmdarray[0]  = "UNKNOWN CODE!";
-$cmdarray[1]  = "CREATE TABLE";
-$cmdarray[2]  = "INSERT";
-$cmdarray[3]  = "SELECT";
-$cmdarray[4]  = "CREATE CLUSTER";
-$cmdarray[5]  = "ALTER CLUSTER";
-$cmdarray[6]  = "UPDATE";
-$cmdarray[7]  = "DELETE";
-$cmdarray[8]  = "DROP CLUSTER";
-$cmdarray[9]  = "CREATE INDEX";
-$cmdarray[10] = "DROP INDEX";
-$cmdarray[11] = "ALTER INDEX";
-$cmdarray[12] = "DROP TABLE";
-$cmdarray[13] = "CREATE SEQUENCE";
-$cmdarray[14] = "ALTER SEQUENCE";
-$cmdarray[15] = "ALTER TABLE";
-$cmdarray[16] = "DROP SEQUENCE";
-$cmdarray[17] = "GRANT";
-$cmdarray[18] = "REVOKE";
-$cmdarray[19] = "CREATE SYNONYM";
-$cmdarray[20] = "DROP SYNONYM";
-$cmdarray[21] = "CREATE VIEW";
-$cmdarray[22] = "DROP VIEW";
-$cmdarray[23] = "VALIDATE INDEX";
-$cmdarray[24] = "CREATE PROCEDURE";
-$cmdarray[25] = "ALTER PROCEDURE";
-$cmdarray[26] = "LOCK TABLE";
-$cmdarray[27] = "NO OPERATION";
-$cmdarray[28] = "RENAME";
-$cmdarray[29] = "COMMENT";
-$cmdarray[30] = "AUDIT";
-$cmdarray[31] = "NOAUDIT";
-$cmdarray[32] = "CREATE DATABASE LINK";
-$cmdarray[33] = "DROP DATABASE LINK";
-$cmdarray[34] = "CREATE DATABASE";
-$cmdarray[35] = "ALTER DATABASE";
-$cmdarray[36] = "CREATE ROLLBACK SEGMENT";
-$cmdarray[37] = "ALTER ROLLBACK SEGMENT";
-$cmdarray[38] = "DROP ROLLBACK SEGMENT";
-$cmdarray[39] = "CREATE TABLESPACE";
-$cmdarray[40] = "ALTER TABLESPACE";
-$cmdarray[41] = "DROP TABLESPACE";
-$cmdarray[42] = "ALTER SESSION";
-$cmdarray[43] = "ALTER USE";
-$cmdarray[44] = "COMMIT";
-$cmdarray[45] = "ROLLBACK";
-$cmdarray[46] = "SAVEPOINT";
-$cmdarray[47] = "PL/SQL EXECUTE";
-$cmdarray[48] = "SET TRANSACTION";
-$cmdarray[49] = "ALTER SYSTEM SWITCH LOG";
-$cmdarray[50] = "EXPLAIN";
-$cmdarray[51] = "CREATE USER";
-$cmdarray[25] = "CREATE ROLE";
-$cmdarray[53] = "DROP USER";
-$cmdarray[54] = "DROP ROLE";
-$cmdarray[55] = "SET ROLE";
-$cmdarray[56] = "CREATE SCHEMA";
-$cmdarray[57] = "CREATE CONTROL FILE";
-$cmdarray[58] = "ALTER TRACING";
-$cmdarray[59] = "CREATE TRIGGER";
-$cmdarray[60] = "ALTER TRIGGER";
-$cmdarray[61] = "DROP TRIGGER";
-$cmdarray[62] = "ANALYZE TABLE";
-$cmdarray[63] = "ANALYZE INDEX";
-$cmdarray[64] = "ANALYZE CLUSTER";
-$cmdarray[65] = "CREATE PROFILE";
-$cmdarray[66] = "DROP PROFILE";
-$cmdarray[67] = "ALTER PROFILE";
-$cmdarray[68] = "DROP PROCEDURE";
-$cmdarray[69] = "DROP PROCEDURE";
-$cmdarray[70] = "ALTER RESOURCE COST";
-$cmdarray[71] = "CREATE SNAPSHOT LOG";
-$cmdarray[72] = "ALTER SNAPSHOT LOG";
-$cmdarray[73] = "DROP SNAPSHOT LOG";
-$cmdarray[74] = "CREATE SNAPSHOT";
-$cmdarray[75] = "ALTER SNAPSHOT";
-$cmdarray[76] = "DROP SNAPSHOT";
-$cmdarray[79] = "ALTER ROLE";
-$cmdarray[85] = "TRUNCATE TABLE";
-$cmdarray[86] = "TRUNCATE CLUSTER";
-$cmdarray[88] = "ALTER VIEW";
-$cmdarray[91] = "CREATE FUNCTION";
-$cmdarray[92] = "ALTER FUNCTION";
-$cmdarray[93] = "DROP FUNCTION";
-$cmdarray[94] = "CREATE PACKAGE";
-$cmdarray[95] = "ALTER PACKAGE";
-$cmdarray[96] = "DROP PACKAGE";
-$cmdarray[97] = "CREATE PACKAGE BODY";
-$cmdarray[98] = "ALTER PACKAGE BODY";
-$cmdarray[99] = "DROP PACKAGE BODY";
-$cmdarray[100]= "LOGON";
-$cmdarray[101]= "LOGOFF";
-$cmdarray[102]= "LOGOFF BY CLEANUP";
-$cmdarray[103]= "SESSION REC";
-$cmdarray[104]= "SYSTEM AUDIT";
-$cmdarray[105]= "SYSTEM NOAUDIT";
-$cmdarray[106]= "AUDIT DEFAULT";
-$cmdarray[107]= "NOAUDIT DEFAULT";
-$cmdarray[108]= "SYSTEM GRANT";
-$cmdarray[109]= "SYSTEM REVOKE";
-$cmdarray[110]= "CREATE PUBLIC SYNONYM";
-$cmdarray[111]= "DROP PUBLIC SYNONYM";
-$cmdarray[112]= "CREATE PUBLIC DATABASE LINK";
-$cmdarray[113]= "DROP PUBLIC DATABASE LINK";
-$cmdarray[114]= "GRANT ROLE";
-$cmdarray[115]= "REVOKE ROLE";
-$cmdarray[116]= "EXECUTE PROCEDURE";
-$cmdarray[117]= "USER COMMENT";
-$cmdarray[118]= "ENABLE TRIGGER";
-$cmdarray[119]= "DISABLE TRIGGER";
-$cmdarray[120]= "ENABLE ALL TRIGGERS";
-$cmdarray[121]= "DISABLE ALL TRIGGERS";
-$cmdarray[122]= "NETWORK ERROR";
-$cmdarray[123]= "EXECUTE TYPE";
-$cmdarray[157]= "CREATE DIRECTORY";
-$cmdarray[158]= "DROP DIRECTORY";
-$cmdarray[159]= "CREATE LIBRARY";
-$cmdarray[160]= "CREATE JAVA";
-$cmdarray[161]= "ALTER JAVA";
-$cmdarray[162]= "DROP JAVA";
-$cmdarray[163]= "CREATE OPERATOR";
-$cmdarray[164]= "CREATE INDEXTYPE";
-$cmdarray[165]= "DROP INDEXTYPE";
-$cmdarray[167]= "DROP OPERATOR";
-$cmdarray[168]= "ASSOCIATE STATISTICS";
-$cmdarray[169]= "DISASSOCIATE STATISTICS";
-$cmdarray[170]= "CALL METHOD";
-$cmdarray[171]= "CREATE SUMMARY";
-$cmdarray[172]= "ALTER SUMMARY";
-$cmdarray[173]= "DROP SUMMARY";
-$cmdarray[174]= "CREATE DIMENSION";
-$cmdarray[175]= "ALTER DIMENSION";
-$cmdarray[176]= "DROP DIMENSION";
-$cmdarray[177]= "CREATE CONTEXT";
-$cmdarray[178]= "DROP CONTEXT";
-$cmdarray[179]= "ALTER OUTLINE";
-$cmdarray[180]= "CREATE OUTLINE";
-$cmdarray[181]= "DROP OUTLINE";
-$cmdarray[182]= "UPDATE INDEXES";
-$cmdarray[183]= "ALTER OPERATOR";
 if($prevsql == "")
   {
   $prevsql = "---";
@@ -238,108 +126,41 @@ else
   {
   $currsql = preg_replace("/\n|\r/","<br>",$currsql);
   }
+$ownerid = '';
+if($d['OWNERID']=='2147483644')
+  {
+  $d['OWNERID'] = 'invalid';
+  }
+$d['Previous SQL'] = $prevsql;
+$d['Current SQL'] = $currsql;
+if(isset($d['COMMAND'])==TRUE && isset($cmdarray[$d['COMMAND']])===TRUE)
+  {
+  $d['COMMAND'].=' ('.$cmdarray[$d['COMMAND']].')';
+  }
 ?>
-<table cellspacing="1" cellpadding="2" border="0" class="datatable" width="98%" summary="Process details">
-<tr class="td_odd">
-  <td class="td_label">Session address:</td>
-  <td><?php echo(isset($d['SADDR']) ? $d['SADDR'] : '---');?></td>
-</tr>
-<tr class="td_even">
-  <td class="td_label">ProcessID:</td>
-  <td><?php echo($d['PROCESS']);?></td>
-</tr>
-<tr class="td_odd">
-  <td class="td_label">Session Identifier (SID):</td>
-  <td><?php echo(isset($d['SID']) ? $d['SID'] : '---');?></td>
-</tr>
-<tr class="td_even">
-  <td class="td_label" title="Serial#">Session serial number:</td>
-  <td><?php echo(isset($d['SERIALNR']) ? $d['SERIALNR'] : '---');?></td>
-</tr>
-<tr class="td_odd">
-  <td class="td_label">Auditing session ID:</td>
-  <td><?php echo(isset($d['AUDSID']) ? $d['AUDSID'] : '---');?></td>
-</tr>
-<tr class="td_even">
-  <td class="td_label">Owner process address:</td>
-  <td><?php echo(isset($d['PADDR']) ? $d['PADDR'] : '---');?></td>
-</tr>
-<tr class="td_odd">
-  <td class="td_label">Oracle Username (ID):</td>
-  <td><?php echo(isset($d['USERNAME']) ? htmlentities(addslashes($d['USERNAME']." (".$d['USERNR'].")")) : '---');?></td>
-</tr>
-<tr class="td_even">
-  <td class="td_label">Schema Username (ID):</td>
-  <td><?php echo(isset($d['SCHEMANAME']) ? $d['SCHEMANAME']." (".$d['SCHEMANR'].")" : '---');?></td>
-</tr>
-<tr class="td_odd">
-  <td class="td_label">Logon time:</td>
-  <td><?php echo(isset($d['LTIME']) ? $d['LTIME'] : '---');?></td>
-</tr>
-<tr class="td_even">
-  <td class="td_label">Last statement parsed:</td>
-  <td><?php echo(isset($d['COMMAND']) ? $cmdarray[$d['COMMAND']]." (".$d['COMMAND'].")" : '---');?></td>
-</tr>
-<tr class="td_odd">
-  <td class="td_label">Owner ID (if valid):</td>
-  <td><?php echo(isset($d['OWNERID']) ? $d['OWNERID'] : '---');?></td>
-</tr>
-<tr class="td_even">
-  <td class="td_label">Addr. of transaction state obj.:</td>
-  <td><?php echo(isset($d['TADDR']) ? $d['TADDR'] : '---');?></td>
-</tr>
-<tr class="td_odd">
-  <td class="td_label">Address of locking wait for:</td>
-  <td><?php echo(isset($d['LOCKWAIT']) ? $d['LOCKWAIT'] : '---');?></td>
-</tr>
-<tr class="td_even">
-  <td class="td_label">Status of session:</td>
-  <td><?php echo(isset($d['STATUS']) ? $d['STATUS'] : '---');?></td>
-</tr>
-<tr class="td_odd">
-  <td class="td_label">Server type:</td>
-  <td><?php echo(isset($d['SERVER']) ? $d['SERVER'] : '---');?></td>
-</tr>
-<tr class="td_even">
-  <td class="td_label">OS client user name:</td>
-  <td><?php echo(isset($d['OSUSER']) ? htmlentities(addslashes($d['OSUSER'])) : '---');?></td>
-</tr>
-<tr class="td_odd">
-  <td class="td_label">Machine name:</td>
-  <td><?php echo(isset($d['MACHINE']) ? htmlentities(addslashes($d['MACHINE'])) : '---');?></td>
-</tr>
-<tr class="td_even">
-  <td class="td_label">Terminal:</td>
-  <td><?php echo(isset($d['TERMINAL']) ? htmlentities(addslashes($d['TERMINAL'])) : '---');?></td>
-</tr>
-<tr class="td_odd">
-  <td class="td_label">Program name:</td>
-  <td><?php echo(isset($d['PROGRAM']) ? htmlentities(addslashes($d['PROGRAM'])) : '---');?></td>
-</tr>
-<tr class="td_even">
-  <td class="td_label">Module name:</td>
-  <td><?php echo(isset($d['MODULE']) ? htmlentities(addslashes($d['MODULE'])) : '---');?></td>
-</tr>
-<tr class="td_odd">
-  <td class="td_label">Session type:</td>
-  <td><?php echo(isset($d['TYPE']) ? $d['TYPE'] : '---');?></td>
-</tr>
-<tr class="td_even">
-  <td class="td_label">Last SQL query:</td>
-  <td><?php echo(htmlspecialchars(htmlentities(addslashes($prevsql))));?></td>
-</tr>
-<tr class="td_odd">
-  <td class="td_label">Current SQL query:</td>
-  <td><?php echo(htmlentities(addslashes($currsql)));?></td>
-</tr>
-<tr class="td_even">
-  <td class="td_label">Current Action:</td>
-  <td><?php echo(isset($d['ACTION']) ? $d['ACTION'] : '---');?></td>
-</tr>
-<tr class="td_odd">
-  <td class="td_label">Info about Client:</td>
-  <td><?php echo(isset($d['CLIENT_INFO']) ? $d['CLIENT_INFO'] : '---');?></td>
-</tr>
+<table cellspacing="1" cellpadding="4" border="0" class="datatable" width="98%" summary="Process details">
+<tbody>
+<?php
+$lv = 0;
+foreach($d as $key => $val)
+  {
+  if($lv % 2)
+    {
+    $myback = 'td_odd';
+    }
+  else
+    {
+    $myback = 'td_even';
+    }
+  if($val == '') $val = '---';
+  echo("<tr class=\"".$myback."\">\n");
+  echo("  <td>".htmlentities(UCWords(StrToLower(str_replace("_"," ",$key))),ENT_COMPAT,'utf-8')."</td>\n");
+  echo("  <td>".htmlentities($val,ENT_COMPAT,'utf-8')."</td>\n");
+  echo("</tr>\n");
+  $lv++;
+  }
+?>
+</tbody>
 </table>
 <br>
 <?php
